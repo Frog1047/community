2b22142adcdf67cf5b9573689397b909cfff1d39
Frog_1047 <wanghanshi9035@163.com>
2020-02-02 15:49:04 +0800

    init repo

---
+++ src/main/java/xyz/wanghehe/community/CommunityApplication.java
@@ -0,0 +1,16 @@
+package xyz.wanghehe.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Frog
+ */
+@SpringBootApplication
+public class CommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityApplication.class, args);
+    }
+
+}
